54d0cd7edcb56f340a9529341a190c6a2ac37924
Julian Wilson <58565920+juju669@users.noreply.github.com>
2021-10-09 11:54:35 -0400

    api route and html route created

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,15 @@
+// const path = require("path");
+// const express = require("express")
+// const router = express.Router();
+
+// // route to index.html
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/index.html"));
+// });
+
+// // route to notes.html
+// router.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../public/notes.html"));
+// });
+
+// module.exports = router;
